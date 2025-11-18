859e31f465301d19b1168504280176b87a574a4a
Natasha <127nbonilla@frhsd.com>
2025-11-18 14:03:20 -0500

    hi

--- js/script.js
+++ js/script.js
@@ -1,2 +1,22 @@
 var namebox= document.getElementById('nameInput');
 console.log(namebox);
+
+var nameAge= document.getElementById('ageInput');
+console.log(nameAge);
+
+var favColor= document.getElementById('colorInput');
+console.log(favColor);
+
+var favPet = document.querySelectorAll("input[type='radio']");
+console.log(favPet);
+
+var hobbies= document.querySelectorAll("input[type='checkbox']");
+console.log(hobbies);
+
+var button= document.getElementById('submitBtn');
+console.log(button);
+
+var outPut= document.getElementById('outPut');
+console.log(outPut);
+
+const element= document.getElementById('mybtn');
